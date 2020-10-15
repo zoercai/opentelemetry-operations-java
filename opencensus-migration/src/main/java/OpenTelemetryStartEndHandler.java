0e7f2d9eab651101204ab246009f0e1e0467bf92
zoercai <zoc@google.com>
2020-10-15 18:13:07 +1100

    Add message event mapping

--- opencensus-migration/src/main/java/OpenTelemetryStartEndHandler.java
+++ opencensus-migration/src/main/java/OpenTelemetryStartEndHandler.java
@@ -1,8 +1,11 @@
 import io.opencensus.implcore.trace.RecordEventsSpanImpl;
 import io.opencensus.implcore.trace.RecordEventsSpanImpl.StartEndHandler;
 import io.opencensus.trace.Annotation;
+import io.opencensus.trace.MessageEvent;
 import io.opencensus.trace.export.SpanData;
 import io.opencensus.trace.export.SpanData.TimedEvent;
+import io.opentelemetry.common.AttributeKey;
+import io.opentelemetry.common.Attributes;
 import io.opentelemetry.trace.Span;
 
 public class OpenTelemetryStartEndHandler implements StartEndHandler {
@@ -30,6 +33,13 @@ public class OpenTelemetryStartEndHandler implements StartEndHandler {
           annotation.getEvent().getDescription(),
           annotation.getTimestamp().getSeconds() * NANOS_PER_SECOND
               + annotation.getTimestamp().getNanos());
+    }
+    for (TimedEvent<MessageEvent> event : spanData.getMessageEvents().getEvents()) {
+      span.addEvent(String.valueOf(event.getEvent().getMessageId()), Attributes.of(
+          AttributeKey.stringKey("message.event.type"), event.getEvent().getType().toString(),
+          AttributeKey.longKey("message.event.size.uncompressed"), event.getEvent().getUncompressedMessageSize(),
+          AttributeKey.longKey("message.event.size.compressed"), event.getEvent().getCompressedMessageSize()
+      ), event.getTimestamp().getSeconds() * NANOS_PER_SECOND + event.getTimestamp().getNanos());
     }
     span.end();
   }
